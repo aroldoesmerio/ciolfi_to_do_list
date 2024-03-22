d99adbbbb8356ea363d288aa748290b2855cf493
200dev.aoliveira <200dev.aoliveira@bv.com.br>
2024-03-22 19:54:03 -0300

    add todo list page

--- lib/presentation/to_do_list_page.dart
+++ lib/presentation/to_do_list_page.dart
@@ -11,14 +11,71 @@ class _TodoListPageState extends State<TodoListPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('To do List'),
-      ),
-      body: Center(
-        child: Container(
-          color: Colors.lightBlueAccent,
+        body: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: <Widget>[
+        const Text('Lista de tarefas'),
+        Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Row(
+            children: [
+              const Expanded(
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                width: 8,
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.deepPurpleAccent,
+                    padding: const EdgeInsets.all(14)),
+                child: const Icon(
+                  Icons.add,
+                  size: 30,
+                ),
+              ),
+            ],
+          ),
         ),
-      ),
-    );
+        SizedBox(
+          height: MediaQuery.of(context).size.height / 2,
+          width: MediaQuery.of(context).size.width,
+          child: ListView.builder(
+              itemCount: 20,
+              itemBuilder: (BuildContext context, int index) => ListTile(
+                    title: Text(
+                      '${DateTime.now()}',
+                      style: const TextStyle(fontSize: 12),
+                    ),
+                    subtitle: const Text('Devo estudar flutter'),
+                  )),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Row(
+            children: [
+              const Expanded(
+                child: Text('Você possui 3 pendencias'),
+              ),
+              const SizedBox(
+                width: 8,
+              ),
+              ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.deepPurpleAccent,
+                      padding: const EdgeInsets.all(14)),
+                  child: const Text('Limpar tudo')),
+            ],
+          ),
+        ),
+      ],
+    ));
   }
 }
